9d4b03acd0e5942bbf92168c2bfb33f7bba1d5b2
freeman <the.freeman.lab@gmail.com>
2016-08-02 16:07:49 -0400

    update setup

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 
 from setuptools import setup, find_packages
 
-version = '1.1.1'
+version = '1.0.0'
 
 required = open('requirements.txt').read().split('\n')
 
 setup(
-    name='neurofinder',
+    name='mesoscope',
     version=version,
-    description='evaluate neuron finding algorithms',
-    author='freeman-lab',
+    description='process data from the two-photon random access mesoscope',
+    author='sofroniewn',
     author_email='the.freeman.lab@gmail.com',
-    url='https://github.com/freeman-lab/neurofinder-python',
+    url='https://github.com/sofroniewn/mesoscope',
     packages=find_packages(),
     install_requires=required,
-    entry_points = {"console_scripts": ['neurofinder = neurofinder.cli:cli']},
-    long_description='See ' + 'https://github.com/freeman-lab/neurofinder-python',
+    entry_points = {"console_scripts": ['mesoscope = mesoscope.cli:cli']},
+    long_description='See ' + 'https://github.com/sofroniewn/mesoscope',
     license='MIT'
 )
