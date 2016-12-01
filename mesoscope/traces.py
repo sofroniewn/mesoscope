14739c2128dfb8a6416322509fa8dc95107dc269
nick <sofroniewn@gmail.com>
2016-12-01 16:06:22 -0500

    fix mean

--- mesoscope/traces.py
+++ mesoscope/traces.py
@@ -1,17 +1,17 @@
 from numpy import zeros, subtract
 from extraction.model import ExtractionModel
 
-def neuropilModel(model, inner=3, outer=8, mask=True):
+def neuropilModel(model, inner=3, outer=8, mask=True, shape=(512, 512)):
     regionsNeuropil = model.regions.outline(inner, outer)
     if mask:
-        mask = model.regions.mask(fill=[1, 1, 1], base=zeros(mean.shape))[:,:,0]
+        mask = model.regions.mask(fill=[1, 1, 1], base=zeros(shape))[:,:,0]
         regionsNeuropil = regionsNeuropil.exclude(mask)
     return ExtractionModel(regionsNeuropil)
 
 def dff(data, model, neuropil=True, inner=3, outer=8):
     t = model.transform(data).normalize()
     if neuropil:
-        modelNeuropil = neuropilModel(model, inner=inner, outer=outer)
+        modelNeuropil = neuropilModel(model, inner=inner, outer=outer, shape=data.shape[1:])
         tN = modelNeuropil.transform(data).normalize()
         t = t.element_wise(tN, subtract)
     return t
