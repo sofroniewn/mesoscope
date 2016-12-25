77cec4938f4308f71572b4da759b243ff4bf200b
Sofroniew <sofroniewn@gmail.com>
2016-12-25 13:11:25 -0800

    put agg at top

--- mesoscope/commands/summarize.py
+++ mesoscope/commands/summarize.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('Agg')
 import os
 import json
 import click
@@ -11,8 +13,6 @@ from skimage.io import imsave
 from .. import downsample
 from .common import success, status, error, warn, setup_spark
 from showit import image
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
